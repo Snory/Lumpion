57d3933f204c885d9c1326be033912bed4e61eb3
Snory <robtill.in@gmail.com>
2020-07-14 18:11:12 +0200

    init

---
+++ Scripts/ObjectPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefabObject;
+
+    [SerializeField]
+    private int _poolDepth;
+
+    [SerializeField]
+    private bool _canGrow;
+
+    private readonly List<GameObject> _pool = new List<GameObject>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < _poolDepth; i++)
+        {
+
+            _pool.Add(CreateObjectForPool());
+        }
+    }
+
+    public GameObject GetAvaiableObject()
+    {
+        for (int i = 0; i < _pool.Count; i++)
+        {
+            if (!_pool[i].activeInHierarchy)
+            {
+                return _pool[i];
+            }
+        }
+
+        if (_canGrow)
+        {
+            return CreateObjectForPool();
+        }
+
+        return null;
+    }
+
+    private GameObject CreateObjectForPool()
+    {
+        GameObject pooledObject = Instantiate(_prefabObject);
+        pooledObject.SetActive(false);
+
+        return pooledObject;
+    }
+
+}
